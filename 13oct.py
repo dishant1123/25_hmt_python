bd1182319103ffac648f60bcca9718d6350f387c
dishant1123 <dishantshah1417@gmail.com>
2025-10-13 12:40:49 +0530

    ..

--- 13oct.py
+++ 13oct.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 data = {
     id :[101,102,103,104,105],
-    "name":["lalit","het","amit","smit","jalay"],
+    "name":["aryaman","het","amit","smit","jalay"],
     "age":[20,21,23,24,19],
     "marks":[90,91,88,86,99],
     "city":["chennai","delhi","mumbai","bangalore","kolkata"]
@@ -22,4 +22,24 @@ print(df.sort_values(["marks","age"],ascending=[True,True]))
 # using filter  : 
 # print(df[df["marks"]>88])
 
-print(df.query("marks>88"))
+# print(df.query("marks>88"))
+
+# print(df.sample(3))  # random 3 row print  
+
+# sql  :  top  3 by  marks 
+"""a= df.nlargest(3,"marks")
+print(a)
+"""
+# bottom  3 : 
+"""b =df.nsmallest(3,"marks")
+print(b)
+"""
+
+# give me  row  like name  start  with "a"  : 
+
+
+# a=df[df["name"].str.startswith("a")]
+# print(a)
+
+b =df.query("marks > 88 and age >15" )
+print(b)
